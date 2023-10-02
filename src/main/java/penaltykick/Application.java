905d680462ad2e5bfbd5f4a6a70117a4fbea3d11
mainfn <_@mainfn.dev>
2023-10-02 23:35:02 +0900

    init

--- src/main/java/penaltykick/Application.java
+++ src/main/java/penaltykick/Application.java
@@ -1,8 +1,8 @@
 package penaltykick;
 
-public class Application {
+public final class Application {
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+  public static void main(final String[] args) {
+    // TODO: 프로그램 구현
+  }
 }
